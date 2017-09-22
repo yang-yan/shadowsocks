fd71ea3af07c1fc5bb0738ff20529261e12e505d
Yang Yan <yan.yang@everydayratings.com>
2017-09-22 19:11:26 +0800

    delete

--- js/main.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($){
-	//open-close submenu on mobile
-	$('.sse-main-nav').on('click', function(event){
-		if($(event.target).is('.sse-main-nav')) $(this).children('ul').toggleClass('is-visible');
-	});
-});
